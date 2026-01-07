88679831559544bb624039ccfd43a09b27d202e5
Nathan-Camredon <nathan.camredon@laplateforme.io>
2026-01-07 10:07:33 +0100

    tupple update

--- printime.py
+++ printime.py
@@ -2,54 +2,64 @@
 import datetime
 import keyboard
 import time
+
 #------ functions ------
 def imput_time():
     """
     Function that allows you to choose a departure time on the clock
     """
-    global h,m,s
     while True:
         try : 
             h = int(input("Enter an hour 0/24 : "))
             m = int(input("Enter a minute  : "))
             s = int(input("Enter a second  : "))
             if (0 <= h < 24) and (0 <= m < 60) and (0 <= s < 60):
-                break
+                return (h, m, s)
             print("Please enter a valid time")
         except:
             print("Please enter only number")
+
 def print_time():
     """
     Function that print time from the system clock
     """
     while True:
         hours = datetime.datetime.now()
-        print(f"Press 'A' to stop. {hours.strftime("%H:%M:%S")}", end="\r")
+        print(f"Press 'A' to stop. {hours.strftime('%H:%M:%S')}", end="\r")
         time.sleep(1)
         if keyboard.is_pressed('a'):
-            choice()
+            return
 
-def set_time(h,m,s):
+def set_time(time_tuple):
     """
     Docstring for set_time
     
     Function that print time from a set time
     """
     while True:
-        print(f"Press 'A' to stop. /n {h:02}:{m:02}:{s:02}", end="\r")
+        h, m, s = time_tuple
+        
+        print(f"Press 'A' to stop. {h:02}:{m:02}:{s:02}   ", end="\r")
+        
         if keyboard.is_pressed('a'):
-            choice()
+            return
+            
         temp = h * 3600 + m * 60 + s
         temp += 1
         h = (temp // 3600) % 24
         m = (temp % 3600) // 60
         s = temp % 60
+        
         time.sleep(1)
+        time_tuple = (h, m, s)
 
 def choice():
     imput = input("Do you want to set the time? (y/n): ")
     if imput == "y":
-        imput_time()
-        set_time(h,m,s)
+        mon_tuple = imput_time()
+        set_time(mon_tuple)
     else:
         print_time()
+
+#------ main ------
+choice()
